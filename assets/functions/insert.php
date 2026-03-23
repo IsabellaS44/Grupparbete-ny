61e963275c69d20a83807ea22c90389d10b33597
Isabella Sahlmén <isabella.sahlmen0031@stud.hkr.se>
2026-03-23 10:52:10 +0000

    Bellas första commit

---
+++ assets/functions/insert.php
@@ -0,0 +1,22 @@
+<?php
+// Checks whether the registration button has been pressed
+if (isset($_POST['dela'])) {
+// Creates a query
+$sql = '
+INSERT INTO Reviews (user_id, review_title, review_content, created_at)
+VALUES (0, :review_title, :review_content, NOW())
+';
+
+// Prepares a query
+$stmt = $dbh->prepare($sql);
+
+// Connects form fields with db containers
+$stmt->bindValue(':review_title', $_POST['review_title']);
+$stmt->bindValue(':review_content', $_POST['review_content']);
+
+// Sends query to database
+if ($stmt->execute()) {
+header('Location: ../../recension.php?action=success');
+exit();
+}
+}
