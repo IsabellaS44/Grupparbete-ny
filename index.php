475accf06f47d72ff9d033715b6790cde56a5288
Angelika Ruzbarsky <angelika.ruzbarsky0067@stud.hkr.se>
2026-03-25 14:37:29 +0000

    Angelika commit 25/3

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="sv">
 <?php
 // Include header
 include 'assets/css/includes/header.php';
@@ -8,19 +6,51 @@ include 'assets/css/includes/footer.php';
 // Include display errors
 include 'assets/css/includes/display_errors.php';
 ?>
+<!DOCTYPE html>
+<html lang="sv">
 
 <head>
-    <meta charset="utf-8">
-    <title>Wisdoom</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <!-- Fontawesome CSS -->
-    <link rel="stylesheet" href="assets/css/all.min.css">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="assets/css/album.css">
+    <meta charset="UTF-8">
+    <title>Min sida</title>
 </head>
 
 <body>
+
+    <section class="pt-5">
+        <div class="container text-center">
+            <?php
+            // Checks if actions is set
+            if (isset($_GET['action'])) {
+
+                // Checks what action is set to
+                switch ($_GET['action']) {
+                    case 'empty':
+                        echo '
+            <div class="alert alert-warning">
+                Du har inte angett en e-postadress eller lösenord.
+            </div>
+            ';
+                        break;
+                    case 'error':
+                        echo '
+            <div class="alert alert-danger">
+                Du har angett fel e-postadress eller lösenord.
+            </div>
+            ';
+                        break;
+                    case 'logout':
+                        echo '
+            <div class="alert alert-danger">
+                Du har loggats ut.
+            </div>
+            ';
+                        break;
+                }
+            }
+            ?>
+        </div>
+    </section>
+
 </body>
 
 </html>
